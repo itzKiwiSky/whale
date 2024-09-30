fcd7a7652e206864f48ac8e1ea19b91be8abefd1
KiwiSky <neonixbuilds@gmail.com>
2024-09-30 14:53:41 -0300

    pachakge itself XD

---
+++ whale/init.lua
@@ -0,0 +1,12 @@
+require("whale.src.utils.Formattable")
+require("whale.src.utils.Math")
+require("whale.src.utils.StringTools")
+require("whale.src.utils.Switch")
+require("whale.src.utils.TermColors")
+require("whale.src.utils.ColoredWrite")
+argparse = require 'whale.src.argparse'
+
+local p = argparse("command")
+local a = p:parse()
+
+print("aa")
