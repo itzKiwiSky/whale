86d7243cc1a174f164076158bf3047f38cd15ddd
KiwiSky <neonixbuilds@gmail.com>
2024-09-30 14:36:47 -0300

    first

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
-print("world")
+require("src.utils.Formattable")
+require("src.utils.Math")
+require("src.utils.StringTools")
+require("src.utils.Switch")
+require("src.utils.TermColors")
+require("src.utils.ColoredWrite")
+argparse = require 'src.argparse'
+
+local function main(args)
+    local p = argparse("command")
+    local a = p:parse()
+end
+
+main(arg)
