efd4b3af3bf0764624ca8dd315df01e10a383654
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-09-30 18:46:03 -0500

    consistency vibe check

--- whale/init.lua
+++ whale/init.lua
@@ -4,14 +4,16 @@ require("whale.src.utils.StringTools")
 require("whale.src.utils.Switch")
 require("whale.src.utils.TermColors")
 require("whale.src.utils.ColoredWrite")
-lfs = require 'lfs'
-language = require 'whale.src.utils.Language'
-command = require 'whale.src.Command'
-help = require 'whale.src.Help'
+
+local lfs = require('lfs')
+local serpent = require("serpent")
+local language = require("whale.src.utils.Language")
+local command = require("whale.src.Command")
+local help = require("whale.src.Help")
 
 local function main(args)
-    command.init()
-    command.run(args)
+	command.init()
+	command.run(args)
 end
 
 main(arg)
